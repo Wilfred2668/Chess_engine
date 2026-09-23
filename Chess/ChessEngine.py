c587d2dd920f65a61cb37fb9588b07342601f58b
Wilfred2668 <wilfred2668@gmail.com>
2026-09-24 02:34:16 +0530

    feat: implemented king moves generation function

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -184,8 +184,16 @@ class GameState():
     Generate all King moves
     '''
     def getKingMoves(self, r, c, moves):
-        pass
-    
+        kingMoves = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
+        allyColor = 'w' if self.whiteToMove else 'b'
+        for m in kingMoves:
+            endRow = r + m[0]
+            endCol = c + m[1]
+            if 0 <= endRow < 8 and 0 <= endCol < 8:
+                endPiece = self.board[endRow][endCol]
+                if endPiece[0] != allyColor:
+                    moves.append(Move((r, c), (endRow,endCol), self.board))
+        
 
 class Move():
 
