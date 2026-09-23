aef3fb7cd71cde54d53a3adbf6234ef618440b08
Wilfred2668 <wilfred2668@gmail.com>
2026-09-24 02:23:00 +0530

    feat: implemented bishop moves generation function

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -155,7 +155,23 @@ class GameState():
     Generate all Bishop moves
     '''
     def getBishopMoves(self, r, c, moves):
-        pass
+        directions = [(-1,1),(1,-1),(-1,-1),(1,1)]
+        enemyColor = 'b' if self.whiteToMove else 'w'
+        for d in directions:
+            for i in range (1,8):
+                endRow = r + d[0] * i
+                endCol = c + d[1] * i
+                if 0 <= endRow < 8 and 0 <= endCol < 8:
+                    endPiece = self.board[endRow][endCol]
+                    if endPiece == '--':
+                        moves.append(Move((r, c), (endRow,endCol), self.board))
+                    elif endPiece[0] == enemyColor:
+                        moves.append(Move((r, c), (endRow,endCol), self.board))
+                        break
+                    else:
+                        break
+                else:
+                    break
 
     '''
     Generate all Queen moves
